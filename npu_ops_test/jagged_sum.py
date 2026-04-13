c86306c8a2ef6cecb629ac670c0de7e50d938792
huangjianbao <huangjianbao2@huawei.com>
2026-04-13 08:17:23 +0000

    debug

--- npu_ops_test/jagged_sum.py
+++ npu_ops_test/jagged_sum.py
@@ -28,9 +28,15 @@ import helion.language as hl
 
 
 # %%
-@helion.kernel(    
+# @helion.kernel(    
+#     autotune_ignore_errors=True,
+#     autotune_effort="full"
+# )
+@helion.kernel(
+    config=helion.Config(block_sizes=[8, 2, 32], indexing=['pointer', 'pointer', 'pointer', 'pointer'], load_eviction_policies=['', '', ''], num_stages=None, 
+num_warps=None, pid_type='flat', range_flattens=[None, False, False], range_multi_buffers=[None, None, True], range_num_stages=[0, 4, 0], range_unroll_factors=[0, 3, 2], range_warp_specializes=[]),
     autotune_ignore_errors=True,
-    autotune_effort="full"
+    autotune_effort="none"
 )
 def jagged_sum_kernel(
     x_data: torch.Tensor,
